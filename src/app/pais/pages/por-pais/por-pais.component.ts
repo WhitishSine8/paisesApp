fb45d8f989f8ffc9cbe02d5ce8788b2df458f196
Miguel Villanueva <miguel.villanuevara@gmail.com>
2022-01-20 00:37:13 -0600

    Actualización de los errores
    Ahora sale un error si no se encuentra un resultado de la búsqueda
    Se cambió además el link de la api a "https://restcountries.com/v3.1"

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -4,20 +4,24 @@ import { PaisService } from '../../services/pais.service';
 @Component({
   selector: 'app-por-pais',
   templateUrl: './por-pais.component.html',
-  styles: [
-  ]
+  
 })
 export class PorPaisComponent {
 
-  termino:string = ""
-  constructor( private paisService:PaisService) { }
+  termino:string = "";
+  hayError:boolean = false;
+  
+  constructor( private paisService:PaisService ) { }
 
   buscar() {
-    console.log(this.termino)
-    this.paisService.buscarPais(this.termino).subscribe(respuesta => {
-      console.log(respuesta)
-    })
-  }
+    this.hayError = false;
+    console.log(this.termino);
 
+    this.paisService.buscarPais( this.termino ).subscribe( (resp) => {
+      console.log(resp);
+    }, (err) => {
+      this.hayError = true;
+    });
+  }
 }
 //private apiUrl: string = 'https://restcountries.com/v2';
--- src/app/pais/services/pais.service.ts
+++ src/app/pais/services/pais.service.ts
@@ -1,18 +1,17 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
+import { catchError } from 'rxjs/operators'
 
 @Injectable({
   providedIn: 'root'
 })
 export class PaisService {
   
-  private _apiUrl:string = "https://restcountries.com/v2/"
-
+  private _apiUrl:string = "https://restcountries.com/v3.1"
   constructor( private http:HttpClient ) { }
 
   buscarPais(termino:string):Observable<any> {
-
     const url = `${this._apiUrl}/name/${termino}`
     return this.http.get(url);
   }
