7ce89233a8940e52c2057988b349b5647a44c066
Miguel Villanueva <miguel.villanuevara@gmail.com>
2022-01-19 22:15:34 -0600

    Actualización de ver pais
    Se trabajo en la interfaz.

--- src/app/pais/pais.module.ts
+++ src/app/pais/pais.module.ts
@@ -1,11 +1,13 @@
+//Importaciones externas
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+//Importaciones internas
 import { PorCapitalComponent } from './pages/por-capital/por-capital.component';
 import { PorPaisComponent } from './pages/por-pais/por-pais.component';
 import { PorRegionComponent } from './pages/por-region/por-region.component';
 import { VerPaisComponent } from './pages/ver-pais/ver-pais.component';
-
-
 
 @NgModule({
   declarations: [
@@ -15,7 +17,8 @@ import { VerPaisComponent } from './pages/ver-pais/ver-pais.component';
     VerPaisComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule
   ],
   exports: [
     PorCapitalComponent,
