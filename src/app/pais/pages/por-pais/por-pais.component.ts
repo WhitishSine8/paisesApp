33990c0bc438d1658b7bb0a12fecc189dc3d9ea6
Miguel Villanueva <miguel.villanuevara@gmail.com>
2022-02-02 22:15:17 -0600

    Actualización de las tablas
    Se muestran ahora los resultados de las tablas.

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -1,4 +1,5 @@
 import { Component} from '@angular/core';
+import { Country } from '../../interfaces/pais-interface';
 import { PaisService } from '../../services/pais.service';
 
 @Component({
@@ -10,6 +11,7 @@ export class PorPaisComponent {
 
   termino:string = "";
   hayError:boolean = true;
+  paises:Country[] = [];
   
   constructor( private paisService:PaisService ) { }
 
@@ -17,10 +19,13 @@ export class PorPaisComponent {
     this.hayError = true;
     console.log(this.termino);
 
-    this.paisService.buscarPais( this.termino ).subscribe( (resp) => {
-      console.log(resp);
+    this.paisService.buscarPais( this.termino ).subscribe( (paises) => {
+      console.log(paises);
+      this.paises = paises;
+      
     }, (err) => {
       this.hayError = false;
+      this.paises = [];
     });
   }
 }
